ec6f92eabec0e3be36df9deb733a6d7deab812df
stoledcat <jetnavo@yandex.ru>
2025-11-19 22:11:35 +0300

    Добавлено приветствие новых пользователей

--- handlers/other.py
+++ handlers/other.py
@@ -1,8 +1,10 @@
 from random import choice
+import asyncio
 
 from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ChatMemberUpdated
+from aiogram.filters import IS_MEMBER, IS_NOT_MEMBER, ChatMemberUpdatedFilter
 
 from lexicon.lexicon import LEXICON, approved, not_approved
 
@@ -25,3 +27,18 @@ async def process_sent_voice(message: Message):
         await message.reply(text=choice(approved))
     else:
         await message.reply(text=choice(not_approved))
+
+
+@router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+async def on_user_joined(event: ChatMemberUpdated):
+    user = event.new_chat_member.user
+    user_id = user.id
+    user_first_name = user.first_name
+    await asyncio.sleep(3)
+    chat_id = event.chat.id
+    await event.bot.send_message(
+        chat_id=chat_id,
+        text=f"Добро пожаловать, <a href='tg://user?id={user_id}'>{user_first_name}</a>!\n"
+        "Нажми команду /start, чтобы записаться в челлендж.",
+        parse_mode="HTML",
+    )
