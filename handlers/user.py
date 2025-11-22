9d19514f2ce520c023b7ee87f3ab400acc517782
stoledcat <jetnavo@yandex.ru>
2025-11-22 23:26:42 +0300

    Добавлена обработка повторных записей в челлендж

--- handlers/user.py
+++ handlers/user.py
@@ -1,11 +1,11 @@
+from datetime import datetime
 from random import choice
 
+import aiosqlite
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
-from aiogram.methods import SendMessage
 from aiogram.types import Message
 
-from create_dp import dp
 from lexicon.lexicon import LEXICON, start
 
 # инициализировать роутер уровня модуля
@@ -15,12 +15,42 @@ router = Router()
 # Этот хэндлер будет срабатывать на команду "/start"
 @router.message(CommandStart())
 async def process_start_command(message: Message):
-    # print(message.model_dump_json(indent=4, exclude_none=True))
-    await message.reply(text=choice(start))
+    date = datetime.now()
+    formatted_datetime = date.strftime("%d.%m.%Y %H:%M:%S")
+    try:
+        async with aiosqlite.connect("app/vplanke.db") as db:
+            user = message.from_user
+            await db.execute(
+                """
+                INSERT INTO users (
+                    user_id,
+                    username,
+                    first_name,
+                    last_name,
+                    joined_at,
+                    left_at,
+                    chat_id,
+                    is_member
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                """,
+                (
+                    user.id,
+                    user.username,
+                    user.first_name,
+                    user.last_name,
+                    formatted_datetime,
+                    None,
+                    message.chat.id,
+                    1,
+                ),
+            )
+            await db.commit()
+        await message.reply(text=choice(start))
+    except aiosqlite.IntegrityError:
+        await message.reply(text="Ты уже участвуешь в челлендже.")
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
 @router.message(Command(commands="help"))
 async def process_help_command(message: Message):
-    # print(message.model_dump_json(indent=4, exclude_none=True))
     await message.reply(text=LEXICON["/help"])
