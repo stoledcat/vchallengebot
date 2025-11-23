fb13014e874cb5b70e9b3ee1b7332a7e3060ddc3
stoledcat <jetnavo@yandex.ru>
2025-11-23 15:55:44 +0300

    Добавлена проверка наличия пользователя в базе, а также является ли он участником челленджа

--- handlers/user.py
+++ handlers/user.py
@@ -12,42 +12,69 @@ from lexicon.lexicon import LEXICON, start, already_started
 router = Router()
 
 
+
 # Этот хэндлер будет срабатывать на команду "/start"
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     date = datetime.now()
-    iso_format = date.strftime("%Y-%m-%d %H:%M:%S")
+    iso_date = date.strftime("%Y-%m-%d %H:%M:%S")
     try:
         async with aiosqlite.connect("app/vplanke.db") as db:
             user = message.from_user
-            await db.execute(
-                """
-                INSERT INTO users (
-                    user_id,
-                    username,
-                    first_name,
-                    last_name,
-                    joined_at,
-                    left_at,
-                    chat_id,
-                    is_member
-                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-                """,
-                (
-                    user.id,
-                    user.username,
-                    user.first_name,
-                    user.last_name,
-                    iso_format,
-                    None,
-                    message.chat.id,
-                    1,
-                ),
-            )
-            await db.commit()
-        await message.reply(text=choice(start))
-    except aiosqlite.IntegrityError:
-        await message.reply(text=choice(already_started))
+            user_id = user.id
+            # Проверить, есть ли пользователь в базе данных
+            async with db.execute(
+                "SELECT is_member FROM users WHERE user_id = ?", (user_id,)
+            ) as cursor:
+                row = await cursor.fetchone()
+
+            if not row:
+                await db.execute(
+                    """
+                    INSERT INTO users (
+                        user_id,
+                        username,
+                        first_name,
+                        last_name,
+                        joined_at,
+                        left_at,
+                        chat_id,
+                        is_member
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                    """,
+                    (
+                        user.id,
+                        user.username,
+                        user.first_name,
+                        user.last_name,
+                        iso_date,
+                        None,
+                        message.chat.id,
+                        1,
+                    ),
+                )
+                await db.commit()
+                await message.reply(text=choice(start))
+            elif row[0] == 0:
+                await db.execute(
+                    """
+                    UPDATE users
+                    SET joined_at = ?, is_member = ?
+                    WHERE user_id = ?
+                    """,
+                    (iso_date, 1, user.id),
+                )
+                await db.commit()
+                await message.reply(text=choice(start))
+            else:
+                await message.reply(text=choice(already_started))
+    except aiosqlite.IntegrityError as e:
+        # Логируем ошибку или информируем пользователя
+        print(f"Integrity error: {e}")
+        await message.reply(
+            "Произошла ошибка при работе с базой данных.\n"
+            "@stoledcat уже спешит на помощь (наверное)."
+        )
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
