98f0537e2af296a9a83d3969777257709f2605b8
stoledcat <jetnavo@yandex.ru>
2025-12-07 21:34:01 +0300

    Изменение структуры базы

--- db/db_scheme.py
+++ db/db_scheme.py
@@ -1,6 +1,6 @@
 events = """
 CREATE TABLE IF NOT EXISTS events (
-    id BIGSERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     chat_title TEXT NOT NULL,
     user_id BIGINT NOT NULL REFERENCES users(user_id),
     username TEXT,
@@ -12,26 +12,31 @@ CREATE TABLE IF NOT EXISTS events (
 
 users = """
 CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT PRIMARY KEY NOT NULL,
-    username TEXT,
+    user_id BIGINT PRIMARY KEY NOT NULL UNIQUE ON CONFLICT IGNORE,
+    username TEXT UNIQUE ON CONFLICT IGNORE,
     first_name TEXT,
     last_name TEXT,
     joined_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
 """
 
 chats = """
 CREATE TABLE IF NOT EXISTS chats (
-    chat_id BIGINT PRIMARY KEY NOT NULL,
+    chat_id BIGINT PRIMARY KEY NOT NULL UNIQUE ON CONFLICT IGNORE,
     chat_title TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
 """
 
 is_member = """
 CREATE TABLE IF NOT EXISTS is_member (
     user_id BIGINT NOT NULL REFERENCES users(user_id),
     chat_id BIGINT NOT NULL REFERENCES chats(chat_id),
+    username TEXT,
+    chat_title TEXT,
     PRIMARY KEY (user_id, chat_id)
 );
 """
